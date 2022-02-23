bcac39c6affc88de455c8700b356dfd5f116e53b
Kim Frans <kimfrans34@gmail.com>
2022-02-23 10:34:49 +0200

    number validation

--- index.js
+++ index.js
@@ -3,55 +3,47 @@ let randomNumber = Math.ceil((Math.random() * 100));
 let inputValue = document.querySelector(".inputGuess");
 const message = document.querySelector(".outputMessage");
 const btn = document.querySelector(".guessbtn");
-const restartBtn = document.querySelector(".restart")
 
 function guessOutcome(){
     const guessedNumber = Number(inputValue.value);
-    console.log(randomNumber);
-    if(guessedNumber > randomNumber){
-        message.innerHTML = "Your guess is too high";
-    }
-    else if(guessedNumber < randomNumber){
-        message.innerHTML = "Your guess is too low";
-    }
-    else {
-        message.innerHTML = `Correct, the secret number is ${randomNumber}`;
+    console.log(randomNumber); 
+
+    if(guessedNumber > 0 && guessedNumber <= 100){
+
+        if(guessedNumber > randomNumber){
+            message.innerHTML = "Your guess is too high";
+        }
+        else if(guessedNumber < randomNumber){
+            message.innerHTML = "Your guess is too low";
+        }
+        else {
+            message.innerHTML = `Correct, the secret number is ${randomNumber}`;
+            setTimeout(function(){
+                randomNumber = Math.ceil((Math.random() * 100));
+                    message.innerHTML = "guess the new number";
+            }, 5000);
+        }
+    
         setTimeout(function(){
-            randomNumber = Math.ceil((Math.random() * 100));
-                message.innerHTML = "guess the new number";
-        }, 5000);
-        // newRandomNumber()
+            message.innerHTML = "";
+        }, 2000);
+
+    }
+    else{
+        message.innerHTML = "Plese enter a number between 1 - 100";
     }
 
-    setTimeout(function(){
-		message.innerHTML = "";
-	}, 2000);
 }
 
 btn.addEventListener('click', guessOutcome)
 
 function newGame(){
-    // setTimeout(function(){
-	// 	message.innerHTML = "A new game has started!";
-	// }, 5000);
-    message.innerHTML = "A new game has started!";
+    setTimeout(function(){
+		message.innerHTML = "A new game has started!";
+	}, 3000);
+    // message.innerHTML = "A new game has started!";
     
-    // guessOutcome();
 }
 window.onload = newGame()
 
-// restartBtn.addEventListener('click', newGame)
-
-// function newRandomNumber(){
-//     const guessedNumber = Number(inputValue.value);
-//     if(guessedNumber == randomNumber){
-//         console.log("match");
-//         randomNumber
-//         console.log(randomNumber);
-//         setTimeout(function(){
-//             message.innerHTML = "guess the new number";
-//         }, 5000);
-//     }
-    
-// }
 
